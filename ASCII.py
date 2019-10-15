60a1610aa625700a1297b0ed847f7a36ee3ca832
Vedvyas Kalupnath <52536911+Vedvyas-Kalupnath@users.noreply.github.com>
2019-10-15 20:26:50 +0400

    Add files via upload

---
+++ ASCII.py
@@ -0,0 +1,7 @@
+print("Find Value of a specific character in ASCII")
+print("")
+
+a = str(input("Enter a single character: "))
+
+# print the ASCII value of assigned character in c
+print("The ASCII value of '" + a + "' is", ord(a))
